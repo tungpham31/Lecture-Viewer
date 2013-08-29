29910df26b9a7c023c49e81b210654acc5ec7bd8
Tim Richards <tim.d.richards@gmail.com>
2013-08-29 07:02:22 -0400

    added register library stub

---
+++ server/lib/register/register.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Q    = require('q');
+var User = require('models').User;
+
+// Checks the user against the roster for a course.
+function checkUserRegistration(user, semester, course) {
+
+}
+
+// Registers a user with a course.
+function registerUser(user, semester, course) {
+
+}
